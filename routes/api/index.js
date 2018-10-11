07e97d8b61162e04b0dfb2eebeebb14624cb2fab
hiwot nega <hiwotmanaye2@gmail.com>
2018-10-11 00:34:11 -0700

    changed welcome message

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 router.get('/', function(req, res, next){
     // res.send('hello from index');
-    res.render('index', { title: 'Profile', message: 'Welcome to Kalstagram'});
+    res.render('index', { title: 'Profile', message: 'Welcome to Coolstagram'});
 });
 module.exports = router;
